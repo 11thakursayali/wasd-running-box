059c130cf9e7e6dfcf3490fd67785c5fa532332d
11thakursayali <97630853+11thakursayali@users.noreply.github.com>
2022-02-23 14:42:33 +0530

    task done

---
+++ src/components/SimpleButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./SimpleButton.module.css";
+const SimpleButton = (props) => {
+    return (<div onClick={props.onClick}>
+      <button className={classes.basic}>Add</button>
+    </div>);
+};
+export default SimpleButton;
